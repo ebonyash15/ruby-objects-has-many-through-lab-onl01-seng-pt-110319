5f55cf5f6aa1f004c96af6c3a183a3b7d3910010
Ebony Sharrock <ebonysharrock@gmail.com>
2019-12-15 20:47:31 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,6 +1,25 @@
 class Doctor
-  @@all=[]
+  @@all=[] 
+  attr_accessor :name
   def self.all
     @@all
+  end
+  def initialize(name)
+    @name=name
+    @@all<<self
+  end
+  def new_appointment(patient, date)
+    appt=Appointment.new(patient, date)
+    appt.doctor=self
+  end
+  def appointments
+    Appointment.all.select do |appt|
+      appt.doctor == self
+    end
+  end
+  def patients
+    self.appointments.collect do |appt|
+      appt.patients
+    end
   end
 end
